042b8f1627c9911e572d888e655d38edab3d7c5f
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-02-07 14:58:26 +0100

    Also fixed a listener loop

--- ElevatorModules/listener.go
+++ ElevatorModules/listener.go
@@ -1,9 +1,14 @@
 package ElevatorModules
 
+import(
+	"time"
+)
+
 func CheckForMaster(){
 	for{
 		//Set up Lister-Timer
 		//Wait for "Im primary" message
 		//If timeout - Become primary
+		time.Sleep(1000000000)
 	}
 }
