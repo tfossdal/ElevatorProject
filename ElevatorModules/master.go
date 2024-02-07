22690584f77f3dcf0eaaa6d750fa140d2ef495db
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-02-07 13:50:31 +0100

    Added port list

--- ElevatorModules/master.go
+++ ElevatorModules/master.go
@@ -3,9 +3,9 @@ package ElevatorModules
 import io "ElevatorProject/elevio"
 
 func Master(){
-	requests := make([][]int, io.numFloors)
-
-	for i := 0; i < io.numFloors; i++{
+	requests := make([][]int, io.NumFloors)
+	
+	for i := 0; i < io.NumFloors; i++{
 		requests[i] = make([]int, io.NumButtons)
 		for j := 0; j < io.NumButtons; j++{
 			requests[i][j] = 0
