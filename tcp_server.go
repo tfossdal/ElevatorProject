fc369fe7d91dbdea5b5b492b45665883c007c25d
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-02-13 11:06:32 +0100

    Added tcp_server test

---
+++ tcp_server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	//io "ElevatorProject/elevio"
+	"fmt"
+	"net"
+)
+
+//const local_IPAdress = "10.24.18.4:33546"
+
+func serverAccept() {
+	addr, err := net.ResolveTCPAddr("tcp", ":33546")
+	if err != nil {
+		panic(err)
+	}
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	fmt.Println("Server is running...")
+	_, err = listener.AcceptTCP()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected")
+	//_, err = s2c_conn.Write(append([]byte("Accepted from group 12"), 0))
+}
+
+func main() {
+	serverAccept()
+}
