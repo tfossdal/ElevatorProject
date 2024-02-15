8e79f1dd52d245d4bebe632c9624c7baaed0101a
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-02-15 14:55:17 +0100

    Testing a listener

--- udp_listener.go
+++ udp_listener.go
@@ -1,9 +1,33 @@
 package main
 
-import(
-	module "ElevatorProject/ElevatorModules"
+import (
+	"fmt"
+	"net"
 )
 
-func main(){
-	module.CheckForPrimary();
+func ListenUDP(port string) {
+	addr, err := net.ResolveUDPAddr("udp4", ":"+port)
+	if err != nil {
+		fmt.Println("Failed to resolve")
+	}
+	conn, err := net.ListenUDP("udp4", addr)
+	if err != nil {
+		fmt.Println("Failed to listen")
+	}
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, recievedAddr, err := conn.ReadFromUDP(buf)
+		senderIP := recievedAddr.IP
+		fmt.Println("Read something")
+		if err != nil {
+			fmt.Println("Failed to listen")
+		}
+		fmt.Println(string(buf[:n]))
+		fmt.Println(senderIP)
+	}
+}
+
+func main() {
+	ListenUDP("29505")
 }
