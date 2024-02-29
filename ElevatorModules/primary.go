bd9e742bbb072713ebaecd23491c4692b445f8a6
LynMaster2000 <hei@gmail.com>
2024-02-29 21:34:03 +0100

    a

--- ElevatorModules/primary.go
+++ ElevatorModules/primary.go
@@ -466,7 +466,7 @@ func ReassignRequests() {
 			return
 		}
 
-		ret, err := exec.Command("../cost_fns/hall_request_assigner/"+hraExecutable, "-i", string(jsonBytes)).CombinedOutput()
+		ret, err := exec.Command("../hall_request_assigner/"+hraExecutable, "-i", string(jsonBytes)).CombinedOutput()
 		if err != nil {
 			fmt.Println("exec.Command error: ", err)
 			fmt.Println(string(ret))
