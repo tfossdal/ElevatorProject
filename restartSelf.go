2ecf780036a33a961f9cb13f5d3150447944545c
ivarhansgard <ivarhan@stud.ntnu.no>
2024-03-12 19:25:42 +0100

    Co-authored-by: tfossdal <tfossdal@users.noreply.github.com>

--- restartSelf.go
+++ restartSelf.go
@@ -1,15 +1,15 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"os/exec"
-// 	"time"
-// )
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
 
-// func main() {
-// 	fmt.Println("Restarting the server")
-// 	time.Sleep(5 * time.Second)
-// 	cmd := exec.Command("gnome-terminal", "-x", "sh", "-c", "go run main.go")
-// 	cmd.Run()
-// 	panic("Restarting")
-// }
+func main() {
+	fmt.Println("Restarting the server")
+	time.Sleep(5 * time.Second)
+	cmd := exec.Command("gnome-terminal", "-x", "sh", "-c", "go run main.go")
+	cmd.Run()
+	panic("Restarting")
+}
