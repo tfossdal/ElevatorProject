1ababce37e68d5700c874e17e195d1e6552a70cf
TFossdal <teodor.fossdal@gmail.com>
2024-03-12 11:17:47 +0100

    a

--- tcp_server.go
+++ tcp_server.go
@@ -1,32 +1,44 @@
 package main
 
-// import (
-// 	//io "ElevatorProject/elevio"
-// 	"fmt"
-// 	"net"
-// )
+import (
+	//io "ElevatorProject/elevio"
+	"fmt"
+	"net"
+)
 
-// //const local_IPAdress = "10.24.18.4:33546"
+//const local_IPAdress = "10.24.18.4:33546"
 
-// func serverAccept() {
-// 	addr, err := net.ResolveTCPAddr("tcp", ":33546")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	listener, err := net.ListenTCP("tcp", addr)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer listener.Close()
-// 	fmt.Println("Server is running...")
-// 	conn, err := listener.AcceptTCP()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Printf("Connected %d", conn.RemoteAddr())
-// 	//_, err = s2c_conn.Write(append([]byte("Accepted from group 12"), 0))
-// }
+func serverAccept() {
+	addr, err := net.ResolveTCPAddr("tcp", ":33546")
+	if err != nil {
+		panic(err)
+	}
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	fmt.Println("Server is running...")
+	conn, err := listener.AcceptTCP()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Connected %d", conn.RemoteAddr())
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	SendAck(string(buf[:n]), conn)
 
-// func main(){
-// 	serverAccept();
-// }
+}
+
+func SendAck(message string, conn *net.TCPConn) {
+	fmt.Println(message)
+	_, err := conn.Write([]byte(message))
+	fmt.Println("Sent ack to primary")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	serverAccept()
+}
