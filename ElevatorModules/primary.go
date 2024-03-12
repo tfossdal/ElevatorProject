687e51bc339c8f87cffe07ee1ee2caed08d72c5c
TFossdal <teodor.fossdal@gmail.com>
2024-03-12 10:03:49 +0100

    a

--- ElevatorModules/primary.go
+++ ElevatorModules/primary.go
@@ -268,7 +268,7 @@ func PrimaryAlive() {
 func SendHallLightUpdate(ticker *time.Ticker) {
 	for {
 		<-ticker.C
-		if PingInternet()==0{
+		if PingInternet() == 0 {
 			return
 		}
 		hallRequestMtx.Lock()
@@ -298,7 +298,8 @@ func SendOrderToBackup(conn *net.TCPConn) {
 				UpdateHallRequests(order[2], order[1], 1)
 			}
 			go SendTurnOnOffLight(order, 1)
-		case order := <-clearOrSendTurnOnOffLight.Write([]byte("c," + fmt.Sprint(order[0]) + "," + fmt.Sprint(order[1]) + "," + fmt.Sprint(order[2]) + ",:"))
+		case order := <-clearOrderCh:
+			_, err := conn.Write([]byte("c," + fmt.Sprint(order[0]) + "," + fmt.Sprint(order[1]) + "," + fmt.Sprint(order[2]) + ",:"))
 			if err != nil {
 				fmt.Print(err)
 				return
